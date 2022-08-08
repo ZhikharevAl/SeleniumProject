e6a88dfebd34f06f53da53aee0f54111f0bb1c01
AlexeyZh <waltafunk@gmail.com>
2022-08-08 13:58:19 +0300

    init

---
+++ saucedemo.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https:\\www.saucedemo.com/")
+
+time.sleep(10)
+webdriver.close()
